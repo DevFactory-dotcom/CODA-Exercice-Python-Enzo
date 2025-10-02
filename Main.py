0b02436b4e8d6f4be05297ae34541141142df3be
Enzo Rouet <enzorouet04@gmail.com>
2025-10-02 11:52:06 +0200

    Exercice 3 terminé

--- Main.py
+++ Main.py
@@ -2,19 +2,7 @@ def exercice1():
     print("Exercice 1 : Bonjour le monde !")
     print("Hello World !")
 
-def main():
-    choix = input("Entrez le numéro de l'exercice à exécuter : ")
-    if choix == "1":
-        exercice1()
-    elif choix == "2":
-        exercice2()
-    elif choix == "3":
-        exercice3()
-    else:
-        print("Exercice non reconnu.")
 
-if __name__ == "__main__":
-    main()
 
 # Exercice 1 :
 
@@ -26,3 +14,24 @@ def exercice2():
 def exercice3() :
     name = input("Quel est ton prénom ?")
     print("Bonjour", name)
+
+# Exercice 3 :
+
+def exerccie4() :
+    print("Première ligne \nDeuxième ligne \nTroisième ligne")
+
+def main():
+    choix = input("Entrez le numéro de l'exercice à exécuter : ")
+    if choix == "1":
+        exercice1()
+    elif choix == "2":
+        exercice2()
+    elif choix == "3":
+        exercice3()
+    elif choix == "4":
+        exerccie4()
+    else:
+        print("Exercice non reconnu.")
+
+if __name__ == "__main__":
+    main()
