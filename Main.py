3b4e6637644aa797d3cb1c2bbb49a6f8816cb16b
Enzo Rouet <enzorouet04@gmail.com>
2025-10-02 11:45:40 +0200

    Exercice 2 terminé

--- Main.py
+++ Main.py
@@ -6,6 +6,10 @@ def main():
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
+    elif choix == "2":
+        exercice2()
+    elif choix == "3":
+        exercice3()
     else:
         print("Exercice non reconnu.")
 
@@ -14,4 +18,11 @@ if __name__ == "__main__":
 
 # Exercice 1 :
 
-print("Hello World")
+def exercice2():
+    print("Hello World")
+
+# Exercice 2 :
+
+def exercice3() :
+    name = input("Quel est ton prénom ?")
+    print("Bonjour", name)
